62fe37ee741e7bc906007a090614512dd0ace461
Tania <89388944+nathaniasan@users.noreply.github.com>
2022-06-15 14:14:14 +0700

    Update uap.cpp

--- uap.cpp
+++ uap.cpp
@@ -1,66 +1,162 @@
 #include <iostream>
-#include <stack>
+#include <conio.h>
 #include <windows.h>
-#define batas 5
+#include <stdlib.h>
+#include <cstdio>
+#include <time.h>
+#include <stack>
+#include <queue>
+#define jmlh 5;
 using namespace std;
 
-stack <int> b;
-int data = 0, jmlh = 10;
-int a[batas] = {2,4,5,6,7};
+struct Tampilan{
+    stack<string> obat;
+    int data = 0;
 
-void daftar(){
-    
+    void clear(){
+        system("cls");
+    }
 
-    for(int i = 0; i < 5; i++){
-        cout<< a[i] <<"\n";
-    }
-}
+    void Push(string medic){
+        if(data == 5){
+            cout<<"Penuh";
 
-void Push(int a){
-    if(data == jmlh){
-        cout<<"penuh";
-    }else{
-        data++;
-        b.push(a);
+        }else{
+            data++;
+            obat.push(medic);
+        }
     }
-}
 
-void Pop(){
-    if(data == 0){
-        data--;
-        a[data];
-    }else{
-        b.pop();
+
+    void cetak(stack <string> medkit){
+        while(!medkit.empty()){
+            cout<< medkit.top()<<"\n";
+            medkit.pop();
+        }
     }
-}
-void cetak(stack <int> a){
-    while(!a.empty()){
-        cout<< a.top() <<"\n";
-        a.pop();
-    }
-}
+
+
+};
+
+Tampilan menu;
+void Home();
+void kursor();
+void gotoxy();
+void login();
+void admin();
 
 
 int main(){
-    int angka, c;
-    
+    Home();
+}
 
-    do{
-        system("cls");
-        cetak(b);
-        daftar();
-        cin >> angka;
+void Home(){
+    cout<<"==================\n"; // 18
+    cout<<"| Selamat Datang |\n";
+    cout<<"==================\n";
+    cout<<"|   LOGIN        |\n";
+    cout<<"|   EXIT         |\n";
+    cout<<"==================\n";
+    kursor();
+}
 
-        switch(angka){
-            case 1: 
-                cin>> c;
-                Push(c);
+void gotoxy(int x, int y){
+
+    COORD garis = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), garis);
+}
+
+void kursor(){
+    char arah;
+    int x=2, y=3; // berfungsi untuk menggerakan kursor di void menu
+
+    gotoxy(x,y);
+    cout<<">";
+
+    while(true){
+        arah = getch();
+
+        switch(arah){
+            case 'w':
+                if (y == 3){
+
+                }else{
+                    gotoxy(x,y-=1);
+                    printf(">");
+                    gotoxy(x, y+1);
+                    printf("  ");
+                }
+                gotoxy(x,y);
+                printf(">");
                 break;
 
-            case 2:
-                
-        }
-        
+            case 's': 
+                if (y == 4){
 
-    }while(angka != 100);
+                }else{
+                    gotoxy(x, y+=1);
+                    printf(">");
+                    gotoxy(x, y-1);
+                    printf("  ");    
+                }
+                gotoxy(x,y);
+                printf(">");
+                break;
+            case 'p':
+                if(y==3){
+                    login();
+                }else{
+
+                }
+                    break;
+        }
+    }
+}
+
+void login(){
+    string username;
+    int pass;
+
+    menu.clear();
+    cout<<"Masukkan Username: ";
+    getline(cin, username);
+
+    if(username == "Reza Nur" || username == "Nathania Santa" || username == "Enjelita Aini"){
+        cout<<"Masukkan Password: ";
+        cin>> pass;
+
+        switch(pass){
+            case 2117051057:
+                admin();
+                break;
+            case 211701009:
+                break;
+            case 2117051028:
+                break;
+        }
+    }
+}
+
+void admin(){
+    int pilih;
+    string nama;
+
+    do{
+        menu.clear();
+        menu.cetak(menu.obat);
+        cin>>pilih;
+
+        switch (pilih){
+        case 1:
+            cin>>nama;
+            menu.Push(nama);
+            break;
+
+        default:
+
+            break;
+        }
+
+    }while(pilih != 4);
+
 }
